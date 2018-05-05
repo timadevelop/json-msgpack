7ec657eb96d41e4c81796a76dce8578b46fa50c5
timadevelop <timadevelop@gmail.com>
2018-05-05 21:16:31 +0300

    json encoding works

--- lib/json/encoder.ex
+++ lib/json/encoder.ex
@@ -1,8 +1,64 @@
 defmodule Json.Encoder do
-
-  def encode(x) do
-    :encoded_json
+  def encode(number) when is_number(number) do
+    to_string(number)
   end
 
-end
+  def encode(nil) do
+    "null"
+  end
 
+  def encode(atom) when is_atom(atom) do
+    to_string(atom)
+  end
+
+  def encode(list) when is_list(list) do
+    "[" <> Enum.map_join(list, ",", fn x -> encode(x) end) <> "]"
+  end
+
+  def encode(map) when is_map(map) do
+    "{" <>
+      Enum.map_join(map, ",", fn {key, value} ->
+        encode(key) <> ":" <> encode(value)
+      end) <> "}"
+  end
+
+  def encode(string) when is_bitstring(string) do
+    "\"" <> encode_string(string) <> "\""
+  end
+
+  defp encode_string(<<?"::utf8, rest::binary>>) do
+    "\"" <> encode_string(rest)
+  end
+
+  defp encode_string(<<?\n::utf8, rest::binary>>) do
+    "\\n" <> encode_string(rest)
+  end
+
+  defp encode_string(<<?\t::utf8, rest::binary>>) do
+    "\\t" <> encode_string(rest)
+  end
+
+  defp encode_string(<<?\b::utf8, rest::binary>>) do
+    "\\b" <> encode_string(rest)
+  end
+
+  defp encode_string(<<?\\::utf8, rest::binary>>) do
+    "\\\\" <> encode_string(rest)
+  end
+
+  defp encode_string(<<?\r::utf8, rest::binary>>) do
+    "\\r" <> encode_string(rest)
+  end
+
+  defp encode_string(<<?\f::utf8, rest::binary>>) do
+    "\\f" <> encode_string(rest)
+  end
+
+  defp encode_string(<<>>) do
+    <<>>
+  end
+
+  defp encode_string(<<char::utf8, rest::binary>>) do
+    <<char::utf8>> <> encode_string(rest)
+  end
+end
--- test/json_msgpack_test.exs
+++ test/json_msgpack_test.exs
@@ -3,7 +3,7 @@ defmodule JsonMsgpackTest do
   doctest JsonMsgpack
 
   test "greets the world" do
-    assert JsonMsgpack.hello() == :world
+    assert JsonMsgpack.hello() === :world
   end
 
   test "decode json numbers" do
@@ -18,31 +18,95 @@ defmodule JsonMsgpackTest do
   end
 
   test "decode json strings" do
-    test_string_decoding(["1123", "string12345",
-                          "soadjknoOKJOJO\'asd[pk]",
-                          "-123IKJN IJN - -q3 '/' dskopf/ smd sdm",
-                          "why yes and w'.ay' not? ha1! /\/\/\/\/\/\/\/"])
+    test_string_decoding([
+      "1123",
+      "string12345",
+      "soadjknoOKJOJO\'asd[pk]",
+      "-123IKJN IJN - -q3 '/' dskopf/ smd sdm",
+      "why yes and w'.ay' not? ha1! /\/\/\/\/\/\/\/"
+    ])
+
     assert Json.decode("\"ads12") === {:error, :unexpected_end_of_string}
   end
 
   defp test_string_decoding(arr) do
-    Enum.each(arr, fn 
-      e when is_binary(e) -> 
-        assert JsonMsgpack.decodeJson("\"#{e}\"") === e
+    Enum.each(arr, fn e when is_binary(e) ->
+      assert JsonMsgpack.decodeJson("\"#{e}\"") === e
     end)
   end
 
-
   test "decode json arrays" do
     assert Json.decode("[]") === []
-    assert Json.decode("[[], [[]], [[[[],[]],[]],[[[],[]]]],[],[]]") === [[], [[]], [[[[],[]],[]],[[[],[]]]],[],[]]
+
+    assert Json.decode("[[], [[]], [[[[],[]],[]],[[[],[]]]],[],[]]") === [
+             [],
+             [[]],
+             [[[[], []], []], [[[], []]]],
+             [],
+             []
+           ]
+
     assert Json.decode("[-1.2341, -0.121, 0, \"asdlm\"]") === [-1.2341, -0.121, 0, "asdlm"]
-    assert Json.decode("[-1.2341, [1,2,3,3], -0.121, 0, \"asdlm\", []]") === [-1.2341, [1,2,3,3] , -0.121, 0, "asdlm", []]
+
+    assert Json.decode("[-1.2341, [1,2,3,3], -0.121, 0, \"asdlm\", []]") === [
+             -1.2341,
+             [1, 2, 3, 3],
+             -0.121,
+             0,
+             "asdlm",
+             []
+           ]
+
     assert Json.decode("[1,2,3,4,5, [1234, 1234]") === {:error, :unexpected_end_of_array}
   end
 
   test "decode json objects to maps" do
-    assert Json.decode("{\"key\": [\"value \"], \"what?1\": {\"nothing\" : null}, \"null\" : null}") ===
-             %{"key" => ["value "], "what?1" => %{"nothing" => nil}, "null" => nil}
+    assert Json.decode(
+             "{\"key\": [\"value \"], \"what?1\": {\"nothing\" : null}, \"null\" : null}"
+           ) === %{"key" => ["value "], "what?1" => %{"nothing" => nil}, "null" => nil}
+  end
+
+  test "encode numbers" do
+    # int
+    assert Json.encode(1) === "1"
+    assert Json.encode(-1) === "-1"
+    assert Json.encode(12345) === "12345"
+    # floats
+    assert Json.encode(0.1) === "0.1"
+    assert Json.encode(0.001) === "0.001"
+    assert Json.encode(-0.001) === "-0.001"
+    assert Json.encode(2134.2341) === "2134.2341"
+  end
+
+  test "encode strings" do
+    assert Json.encode("i am an string!") === "\"i am an string!\""
+
+    assert Json.encode("Hi, here: \"is some text\", \n and newline") ===
+             "\"Hi, here: \"is some text\", \\n and newline\""
+  end
+
+  test "encode arrays" do
+    assert Json.encode([]) === "[]"
+
+    assert Json.encode([[], [[]], [[[[], []], []], [[[], []]]], [], []]) ===
+             "[[],[[]],[[[[],[]],[]],[[[],[]]]],[],[]]"
+
+    assert Json.encode([-1.2341, -0.121, 0, "asdlm"]) === "[-1.2341,-0.121,0,\"asdlm\"]"
+
+    assert Json.encode([-1.2341, [1, 2, 3, 3], -0.121, 0, "asdlm", []]) ===
+             "[-1.2341,[1,2,3,3],-0.121,0,\"asdlm\",[]]"
+
+    # assert Json.decode("[1,2,3,4,5, [1234, 1234]") === {:error, :unexpected_end_of_array}
+  end
+
+  test "encode maps" do
+    assert Json.encode(%{"key0" => ["value0"]}) ===
+             "{\"key0\":[\"value0\"]}"
+    assert Json.encode(%{"key0" => ["value0"], "key1" => %{"v" => 3}}) ===
+             "{\"key0\":[\"value0\"],\"key1\":{\"v\":3}}"
+
+    # maps are not ordered in elixir!
+    # assert Json.encode(%{"key" => ["value "], "what?1" => %{"nothing" => nil}, "null" => nil}) ===
+    # "{\"key\":[\"value \"],\"what?1\":{\"nothing\":null},\"null\":null}"
   end
 end
